c2e03e46ef93ac61ddf5da082a9d26ff3f8bdb7e
Moore Crest <158084193+Amosalfa340@users.noreply.github.com>
2024-03-06 12:18:24 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBQeKUH-byA9b1mkSmTHxMf89c7WCoN7HM",
+    authDomain: "maybe-2121.firebaseapp.com",
+    databaseURL: "https://maybe-2121-default-rtdb.firebaseio.com",
+    projectId: "maybe-2121",
+    storageBucket: "maybe-2121.appspot.com",
+    messagingSenderId: "809043994780",
+    appId: "1:809043994780:web:edd786a12b09b3e53fe559",
+    measurementId: "G-N48ZVPYVZN"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const analytics = firebase.analytics();
